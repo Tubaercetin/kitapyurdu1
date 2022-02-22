f9635808e2bcc763d733aea1c60d8e336ae205ad
Tubaercetin <tuba.ercetin@testinium.com>
2022-02-22 23:24:07 +0300

    Initial commit

---
+++ src/test/java/com/testinium/page/LoginPage.java
@@ -0,0 +1,27 @@
+package com.testinium.page;
+
+import com.testinium.methods.Methods;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+
+public class LoginPage {
+    Methods methods;
+
+    public LoginPage(){
+        methods = new Methods();
+    }
+
+    public void login(){
+        methods.click(By.cssSelector(".menu-top-button.login>a"));
+        methods.waitBySeconds(5);
+        methods.sendKey(By.id("login-email"),"tuba.ercetin@testinium.com");
+        methods.waitBySeconds(3);
+        methods.sendKey(By.id("login-password"),"1q2w3e4r5t");
+        methods.waitBySeconds(3);
+        methods.click(By.cssSelector(".ky-btn.ky-btn-orange.w-100.ky-login-btn"));
+        methods.waitBySeconds(15);
+        Assert.assertTrue(methods.isElementVisible(By.cssSelector(".section")));
+
+    }
+}
+
